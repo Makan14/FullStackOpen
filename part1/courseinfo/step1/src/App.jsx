d36ff9e351dad36c89f52a05b54538ad3c9761e2
Makan <m.macalou522@gmail.com>
2026-08-26 20:38:48 +0200

    refactorise component header content et total

--- part1/courseinfo/step1/src/App.jsx
+++ part1/courseinfo/step1/src/App.jsx
@@ -1,59 +1,58 @@
 const App = () => {
-  
-  return(
-
-    <div>
-      <Header/>
-      <Content/> 
-      <Total/>
-    </div>
-  )
-}
-
-// component header
-const Header = () =>{
   const course = 'Half Stack application development';
-
-  return(
-    <div>
-      <h1>{course}</h1> 
-    </div>
-  )
-}
-
-// component content
-const Content = () =>{
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const totalExercises = exercises1 + exercises2 + exercises3
+
+  return(
+
+    <div>
+      <Header course = {course}/> 
+      <Content part1 = {part1} exercises1 = {exercises1}/> 
+      <Content part2 = {part2} exercises2 = {exercises2}/> 
+      <Content part3 = {part3} exercises3 = {exercises3}/> 
+      <Total totalExercises = {totalExercises}/> 
+    </div>
+  )
+}
+
+// component header
+const Header = (props) =>{
 
   return(
     <div>
+      <h1>{props.course}</h1> 
+    </div>
+  )
+}
+
+// component content
+const Content = (props) =>{
+  
+  return(
+    <div>
       <p>
-        {part1} {exercises1}
+        {props.part1} {props.exercises1}
       </p>
       <p>
-        {part2} {exercises2}
+        {props.part2} {props.exercises2}
       </p>
       <p>
-        {part3} {exercises3}
+        {props.part3} {props.exercises3}
       </p>
     </div>
   )
 }
 
 // component total
-const Total = () =>{
-  
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+const Total = (props) =>{
 
   return(         
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>  
+      <p>Number of exercises {props.totalExercises}</p>  
   )
 } 
 
