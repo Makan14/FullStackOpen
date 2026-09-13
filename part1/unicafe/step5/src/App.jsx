ebb1023d670f49e15741cf6db6c85517f4546b4b
Makan <m.macalou522@gmail.com>
2026-09-13 18:15:00 +0200

    move anecdotes folder + fix table

--- part1/unicafe/step5/src/App.jsx
+++ part1/unicafe/step5/src/App.jsx
@@ -39,9 +39,9 @@ const Statistics = (props) =>{
       <StatisticLine text="good" value={props.good}/> 
       <StatisticLine text="neutral" value={props.neutral}/> 
       <StatisticLine text="bad" value={props.bad}/> 
-      <StatisticLine text="all" value={props.good + props.neutral +props. bad}/> 
+      <StatisticLine text="all" value={props.good + props.neutral +props.bad}/> 
       <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)}/> 
-      <StatisticLine text="positif" value={(props.good * 100) / (props.good + props.neutral + props.bad) + " %"}/> 
+      <StatisticLine text="positive" value={(props.good * 100) / (props.good + props.neutral + props.bad) + " %"}/> 
     </div>
   )
 };
--- part1/unicafe/step6/src/App.jsx
+++ part1/unicafe/step6/src/App.jsx
@@ -28,15 +28,18 @@ const Statistics = (props) =>{
   }
 
   return(
-    <tbody>
+    <table>
+      <tbody>
 
-      <StatisticLine text="good" value={props.good}/> 
-      <StatisticLine text="neutral" value={props.neutral}/> 
-      <StatisticLine text="bad" value={props.bad}/> 
-      <StatisticLine text="all" value={props.good + props.neutral +props. bad}/> 
-      <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)}/> 
-      <StatisticLine text="positive" value={(props.good * 100) / (props.good + props.neutral + props.bad) + " %"}/> 
-    </tbody>
+        <StatisticLine text="good" value={props.good}/> 
+        <StatisticLine text="neutral" value={props.neutral}/> 
+        <StatisticLine text="bad" value={props.bad}/> 
+        <StatisticLine text="all" value={props.good + props.neutral +props.bad}/> 
+        <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)}/> 
+        <StatisticLine text="positive" value={(props.good * 100) / (props.good + props.neutral + props.bad) + " %"}/> 
+      </tbody>
+
+    </table>
   )
 };
 
